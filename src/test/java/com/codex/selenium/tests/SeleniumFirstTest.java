66ac3325bc0cce0ad79a94a096c08817c5ceadc6
Mohit <mohitkurmi.official@gmail.com>
2026-06-09 11:16:18 +0530

    First elenium test with Full code

--- src/test/java/com/codex/selenium/tests/SeleniumFirstTest.java
+++ src/test/java/com/codex/selenium/tests/SeleniumFirstTest.java
@@ -1,4 +1,17 @@
 package com.codex.selenium.tests;
 
-public class SeleniumFirstTest {
+import com.codex.selenium.base.SeleniumBaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SeleniumFirstTest extends SeleniumBaseTest {
+
+    @Test
+    public void openExampleWebsite() {
+        driver.get("https://example.com");
+
+        String actualTitle = driver.getTitle();
+
+        Assertions.assertEquals("Example Domain", actualTitle);
+    }
 }
